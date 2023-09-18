9136e99b74d580daee37a50a4318530685bca18a
drinkmonster <berenyibence98@gmail.com>
2023-09-18 21:59:37 +0200

    error page, settings page title set in js

---
+++ error.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="mt-7 max-w-sm mx-auto text-center card">
+        <p class="mt-7 text-7xl font-bold">
+            {{ error.statusCode }}
+        </p>
+        <p class="mt-7 text-6xl">
+            Valami nem jó pali...
+        </p>
+        <p class="mt-7">{{ error.message }}</p>
+        <UButton class="mt-4" size="xl" @click="handleClearError">Vissza a főoldalra...</UButton>
+    </div>
+</template>
+
+<script setup>
+    defineProps(['error'])
+
+    const handleClearError = () => {
+        clearError({ redirect: '/' })
+    }
+</script>
+
+<style scoped>
+
+</style>
--- pages/settings.vue
+++ pages/settings.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script setup>
-
+        useHead({  title: 'Beállítások'})
 </script>
 
 <style lang="scss" scoped>
