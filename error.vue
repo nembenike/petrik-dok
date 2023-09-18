74cc2f83a4d3276c2d8758d3602e3b236e794918
drinkmonster <berenyibence98@gmail.com>
2023-09-18 22:15:29 +0200

    improved error page

--- error.vue
+++ error.vue
@@ -1,13 +1,14 @@
 <template>
-    <div class="mt-7 max-w-sm mx-auto text-center card">
+    <div class="flex justify-center flex-col mt-7 mx-auto text-center w-100">
         <p class="mt-7 text-7xl font-bold">
             {{ error.statusCode }}
         </p>
         <p class="mt-7 text-6xl">
             Valami nem jó pali...
         </p>
+        <img class="mt-8 w-80 self-center" src="https://media.discordapp.net/attachments/968844264471662613/1152904289941979146/280.jpg?width=674&height=676" />
         <p class="mt-7">{{ error.message }}</p>
-        <UButton class="mt-4" size="xl" @click="handleClearError">Vissza a főoldalra...</UButton>
+        <UButton class="mt-4 self-center" size="xl" @click="handleClearError">Vissza a főoldalra...</UButton>
     </div>
 </template>
 
