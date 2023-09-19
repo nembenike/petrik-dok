1cf168c5a7d4490ae07eee60074b5a364caa36da
drinkmonster <berenyibence98@gmail.com>
2023-09-19 15:59:24 +0200

    git test

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
     <Head>
-        <Title>DÖK Webapp</Title>
+        <Title>DÖK App</Title>
     </Head>
     <div class="flex flex-col justify-center items-center">
         <h1 class="text-5xl">Petrik DÖK Webapp!</h1>
