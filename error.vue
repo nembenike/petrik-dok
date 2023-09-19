b8264991780a4a0ff262c29451ece6af1e7bbd93
drinkmonster <berenyibence98@gmail.com>
2023-09-19 21:09:20 +0200

    diff image

--- error.vue
+++ error.vue
@@ -6,7 +6,7 @@
         <p class="mt-7 text-6xl">
             Valami nem jó pali...
         </p>
-        <img class="mt-8 w-80 self-center" src="https://media.discordapp.net/attachments/968844264471662613/1152904289941979146/280.jpg?width=674&height=676" />
+        <img class="mt-8 w-80 self-center" src="https://media.discordapp.net/attachments/528224401783521281/864916793335611422/image0-28-1.gif" />
         <p class="mt-7">{{ error.message }}</p>
         <UButton class="mt-4 self-center" size="xl" @click="handleClearError">Vissza a főoldalra...</UButton>
     </div>
