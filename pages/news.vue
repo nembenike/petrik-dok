504485585d35a17c46e391b25356d7c274112993
Susa Milán <mailtosnek@protonmail.com>
2023-09-20 19:21:21 +0200

    News scaling

--- pages/news.vue
+++ pages/news.vue
@@ -4,7 +4,7 @@
             Hírek
         </title>
     </Head>
-    <div class="grid grid-cols-3 gap-3">
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
         <newsCard />
         <newsCard />
         <newsCard />
