fc01488003329bdea3a9500e3e139595a305e04e
drinkmonster <berenyibence98@gmail.com>
2023-09-21 21:56:00 +0200

    event card

--- components/eventCard.vue
+++ components/eventCard.vue
@@ -1,7 +1,14 @@
 <template>
     <div>
-        <p>{{ event.title }}</p>
-        <NuxtLink :to="`/events/${event.id}`">Visit event</NuxtLink>
+        <UCard>
+            <template #header>
+                <p>{{ event.title }}</p>
+            </template>
+            <p class="truncate">{{ event.description }}</p>
+            <template #footer>
+                <UButton :to="`/events/${event.id}`">Megnézem</UButton>
+            </template>
+        </UCard>
     </div>
 </template>
 
