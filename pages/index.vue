39d351e27d139303125d7b55b7c055ac23149db1
drinkmonster <berenyibence98@gmail.com>
2023-09-24 14:33:29 +0200

    balls

--- pages/index.vue
+++ pages/index.vue
@@ -4,9 +4,6 @@
     </Head>
     <div class="flex flex-col justify-center items-center">
         <h1 class="text-5xl">Petrik DÖK Webapp!</h1>
-        <div class="flex flex-col items-center mt-4">
-            <UButton to="/login" size="xl">Belépés</UButton>
-        </div>
     </div>
 </template>
   
--- pages/settings.vue
+++ pages/settings.vue
@@ -1,14 +1,16 @@
 <template>
     <div>
-        <h1 class="text-center text-2xl">This is the settings page, you can change pfp and password here</h1>
+        <h1 class="text-center text-2xl">Beállítások</h1>
+        <p>Email: {{ user.email }}</p>
     </div>
 </template>
 
 <script setup>
-        useHead({  title: 'Beállítások'})
-        definePageMeta({
-            middleware: ["auth"],
-        });
+    useHead({  title: 'Beállítások'})
+    definePageMeta({
+        middleware: ["auth"],
+    });
+    const user = useSupabaseUser();
 </script>
 
 <style lang="scss" scoped>
