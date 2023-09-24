528a22bb81de248628730a5e7d1a7a7f4da53674
drinkmonster <berenyibence98@gmail.com>
2023-09-24 15:29:28 +0200

    ong

--- pages/login.vue
+++ pages/login.vue
@@ -29,11 +29,15 @@ onMounted(() => {
     }
   })
 })
+definePageMeta({
+    layout: "login",
+    middleware: "login"
+})
 </script>
 
 <template>
-  <div class="max-w-lg mx-auto mt-8">
-    <h1 class="text-3xl font-black text-white">LearnVue Nuxt Demo</h1>
+  <div class="max-w-sm w-full mx-auto mt-8 center-div">
+    <h1 class="text-3xl font-black text-white text-center">DÖK App</h1>
     <form
       @submit.prevent="() => (isSignUp ? signUp() : login())"
       class="flex flex-col gap-2 mt-4"
@@ -67,3 +71,14 @@ onMounted(() => {
     </button>
   </div>
 </template>
+
+<style scoped>
+  .center-div {
+  margin: 0;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  -ms-transform: translate(-50%, -80%); /* Not fully center since i think it's a bit weird. */
+  transform: translate(-50%, -80%); /* Not fully center since i think it's a bit weird. */
+}
+</style>
